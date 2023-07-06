c1c643286616656032757cd28c3ec386f77df11c
Jerome Andrei O. Hontiveros <jrmhntvrs@gmail.com>
2023-07-07 03:59:40 +0800

    get name method

--- Pet/Pet.py
+++ Pet/Pet.py
@@ -6,14 +6,19 @@ class Pet:
         self.__animalType = ""
         self.__age = 0
     
-    # name scheme method
+# name scheme method
+    # sets name
     def set_name(self, name):
         self.__name = name
     
-    # animal type method
+    # gets name
+    def get_name(self):
+        return self.__name
+    
+# animal type method
     def set_animal_type(self, animal_type):
         self.__animal_type = animal_type
     
-    # age method
+# age method
     def set_age(self, age):
         self.__age = age
