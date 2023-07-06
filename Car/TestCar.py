d2bfccea359e417609566b49bdf11cce0ee21b71
Jerome Andrei O. Hontiveros <jrmhntvrs@gmail.com>
2023-07-07 03:42:25 +0800

    acceleration and braking test

--- Car/TestCar.py
+++ Car/TestCar.py
@@ -2,3 +2,15 @@ from Car import Car
 
 # create car object
 car = Car(2023, "Porsche")
+
+# acceleration 
+print("Acceleration Method")
+for i in range(5):
+    car.accelerate()
+    print("Current Speed:", car.get_speed())
+
+# braking
+print("\nBraking Method")
+for i in range(5):
+    car.brake()
+    print("Current Speed:", car.get_speed())
