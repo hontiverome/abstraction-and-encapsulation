3ed65a60c733308d65930920676e4a748fe7874f
Jerome Andrei O. Hontiveros <jrmhntvrs@gmail.com>
2023-07-07 03:02:03 +0800

    initialization

--- Fan.py
+++ Fan.py
@@ -7,3 +7,8 @@ class Fan:
     __medium=2
     __fast=3
     
+    def __init__(self, speed=1,radius=5,color="blue",on=False) -> None:
+        self.__speed = speed
+        self.__radius = radius
+        self.__color = color
+        self.__on = on
