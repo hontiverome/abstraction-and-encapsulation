ad23e2b7776b8299341f2eed3d55bed1ea6e4bfc
Jerome Andrei O. Hontiveros <jrmhntvrs@gmail.com>
2023-07-07 03:58:27 +0800

    name method

--- Pet/Pet.py
+++ Pet/Pet.py
@@ -5,3 +5,7 @@ class Pet:
         self.__name = ""
         self.__animalType = ""
         self.__age = 0
+    
+    # name scheme method
+    def set_name(self, name):
+        self.__name = name
