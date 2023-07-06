d6f33cc3e9e2b63684e8affbfd3ade0bd5ee303c
Jerome Andrei O. Hontiveros <jrmhntvrs@gmail.com>
2023-07-07 03:34:03 +0800

    brake and speed check method

--- Car/Car.py
+++ Car/Car.py
@@ -11,3 +11,15 @@ class Car:
     # method that increases car speed
     def accelerate(self):
         self.__speed += 5
+    
+    # car brake method
+    def brake(self):
+        if self.__speed >= 5:
+            self.__speed -= 5
+        else:
+            self.__speed = 0
+
+    # current speed checker method
+    def get_speed(self):
+        return self.__speed
+    
