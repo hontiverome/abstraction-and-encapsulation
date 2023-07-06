3ed58f17275106f89fc52ed2cc0134b81db1c522
Jerome Andrei O. Hontiveros <jrmhntvrs@gmail.com>
2023-07-07 04:22:06 +0800

    design

--- Car/TestCar.py
+++ Car/TestCar.py
@@ -1,17 +1,21 @@
 from Car import Car
-import time
+import time, pyfiglet
+from termcolor import cprint, colored
 
 # create object
 car = Car(2023, "Porsche")
 
 # acceleration 
-print("Acceleration Method")
+accelerateTitle = pyfiglet.figlet_format("Acceleration", font="lcd")
+cprint("\n" +  "="*50 + "\n" + colored(accelerateTitle), "red", attrs=["bold"])
 for i in range(5):
     time.sleep(0.5)
     car.accelerate()
     print("Accelerating... Current Speed:", car.get_speed())
 
 # braking
+brakingTitle = pyfiglet.figlet_format("Braking", font="lcd")
+cprint("\n" +  "="*50 + "\n" + colored(brakingTitle), "red", attrs=["bold"])
 print("\nBraking Method")
 for i in range(5):
     time.sleep(0.5)
