987fd0db2312139d20af8b377cc1b197a214b143
Jerome Andrei O. Hontiveros <jrmhntvrs@gmail.com>
2023-07-07 04:05:42 +0800

    input

--- Pet/TestPet.py
+++ Pet/TestPet.py
@@ -2,3 +2,8 @@ from Pet import Pet
 import time
 # create pet object
 pet = Pet("", "", "")
+
+# input for pet information 
+pet.setName(input("What is the pet's name? : "))
+pet.setAnimalType(input("What is their type? : "))
+pet.setAge(input("What is their age? : "))
