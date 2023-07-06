c4cc410c414fd59cf3074bd861d7cce74322ba7c
Jerome Andrei O. Hontiveros <jrmhntvrs@gmail.com>
2023-07-07 03:32:08 +0800

    attributes

--- Car/Car.py
+++ Car/Car.py
@@ -2,4 +2,7 @@
 # class creation
 class Car:
     # constructor that initializes car attributes 
-    
+    def __init__(self, year_model, maker):
+        self.__year_model = year_model
+        self.__maker = maker
+        self.__speed = 0
